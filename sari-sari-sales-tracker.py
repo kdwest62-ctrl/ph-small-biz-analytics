c1ea56908719ebb3c8a45b17db3b885d334a7daf
Kear Ibale <kdwest62@gmail.com>
2026-06-06 07:26:58 +0800

    Add menu options for CSV sales tracker

--- sari-sari-sales-tracker.py
+++ sari-sari-sales-tracker.py
@@ -2,4 +2,15 @@ import pandas as pd
 
 path = input("CSV path: ")
 df = pd.read_csv(path)
-print(df.to_string())
+print("1. CSV\n2. Sales\n3. Exit")
+while True:
+    option = input("Select option (number): ")
+    if option == "1":
+        print(df.to_string())
+    elif option == "2":
+        pass
+    elif option == "3":
+        print("Program closed")
+        break
+    else:
+        print("Option not available")
