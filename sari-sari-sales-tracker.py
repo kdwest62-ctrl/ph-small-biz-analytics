f6460f0b15e2ab97e14db33578f312b42d913288
Kear Ibale <kdwest62@gmail.com>
2026-06-06 07:34:00 +0800

    Check if CSV path exists before loading data
    Added path existence check before reading CSV.

--- sari-sari-sales-tracker.py
+++ sari-sari-sales-tracker.py
@@ -1,16 +1,20 @@
 import pandas as pd
+from pathlib import Path
 
-path = input("CSV path: ")
-df = pd.read_csv(path)
-print("1. CSV\n2. Sales\n3. Exit")
-while True:
-    option = input("Select option (number): ")
-    if option == "1":
-        print(df.to_string())
-    elif option == "2":
-        pass
-    elif option == "3":
-        print("Program closed")
-        break
-    else:
-        print("Option not available")
+path = Path(input("CSV path: "))
+if path.exists():
+    df = pd.read_csv(path)
+    print("1. CSV\n2. Sales\n3. Exit")
+    while True:
+        option = input("Select option (number): ")
+        if option == "1":
+            print(df.to_string())
+        elif option == "2":
+            pass
+        elif option == "3":
+            print("Program closed")
+            break
+        else:
+            print("Option not available")
+else:
+    print("Path does not exist")
