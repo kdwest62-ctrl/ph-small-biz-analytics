e970eb23e19e419b8397e818851513e0503d607f
Kear Ibale <kdwest62@gmail.com>
2026-06-09 07:38:51 +0800

    Add revenue option and categories to sales tracker

--- sari-sari-sales-tracker.py
+++ sari-sari-sales-tracker.py
@@ -4,14 +4,25 @@ from pathlib import Path
 path = Path(input("CSV path: "))
 if path.exists():
     df = pd.read_csv(path)
-    print("1. CSV\n2. Sales\n3. Exit")
+    print("1. CSV\n2. Sales\n3. Revenue\n4. Exit")
     while True:
         option = input("Select option (number): ")
         if option == "1":
             print(df.to_string())
         elif option == "2":
-            pass
+            print("a. Rankings | b. Top Seller | c. Low Seller")
+            category = input("Choose category (letter): ")
+            if category == "a":
+                pass
+            elif category == "b":
+                pass
+            elif category == "c":
+                pass
+            else:
+                print("Option not available")
         elif option == "3":
+            pass
+        elif option == "4":
             print("Program closed")
             break
         else:
