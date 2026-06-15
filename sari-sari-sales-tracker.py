758428433fa24f0dcf75c080f56e3e01bdcac0d7
Kear Ibale <kdwest62@gmail.com>
2026-06-16 07:41:06 +0800

    Refactor menu options and improve readability

--- sari-sari-sales-tracker.py
+++ sari-sari-sales-tracker.py
@@ -1,39 +1,28 @@
 import pandas as pd
 from pathlib import Path
 
-path = Path(input("CSV path: "))
+path = Path(input('CSV path: '))
 if path.exists():
     df = pd.read_csv(path)
+    print('Menu')
+    print('1. Data\n2. Sales\n3. Revenue\n4. Profit\n5. Inventory\n6. Exit')
     while True:
-        print("Menu")
-        print("1. Data\n2. Rankings\n3. Exit")
-        option = input("Select option: ")
-        if option == "1":
+        option = input('Select option: ')
+        if option == '1':
             print(df.to_string())
-            print("-" * 8)
-        elif option == "2":
-            print("a. Sales | b. Revenue | c. Profit | d. Inventory")
-            rankings = input("Select rankings: ")
-            if rankings == "a":
-                sales = df.groupby("product")["quantity_sold"].sum().sort_values(ascending=False)
-                print(sales)
-                print("-" * 8)
-            elif rankings == "b":
-                revenue = (df.assign(Revenue=df['quantity_sold'] * df['price'])
-                          .groupby('product')['Revenue']
-                          .sum().sort_values(ascending=False))
-                print(revenue)
-                print("-" * 8)
-            elif rankings == "c":
-                pass
-            elif rankings == "d":
-                pass
-            else:
-                print("Rankings not available")
-        elif option == "3":
-            print("Program closed")
+            print('-' * 8)
+        elif option == '2':
+            pass
+        elif option == '3':
+            pass
+        elif option == '4':
+            pass
+        elif option == '5':
+            pass
+        elif option == '6':
+            print('Program closed')
             break
         else:
-            print("Option not available")
+            print('Option not available')
 else:
-    print("Path does not exist")
+    print('Path does not exist')
