902e540a566f73eda4401c7c8755f0f0d81b1730
Kear Ibale <kdwest62@gmail.com>
2026-06-16 13:30:02 +0800

    Implement sales summary for products
    Added functionality to display total sales for each product based on quantity sold.

--- sari-sari-sales-tracker.py
+++ sari-sari-sales-tracker.py
@@ -12,7 +12,25 @@ if path.exists():
             print(df.to_string())
             print('-' * 8)
         elif option == '2':
-            pass
+            column_data = df['product'].tolist()
+            products = []
+            for entry in column_data:
+                if entry not in products:
+                    products.append(entry)
+            quantity_sold = []
+            for product in products:
+                if column_data.count(product) > 1:
+                    prod = df[df['product'] == product]
+                    sales = prod['quantity_sold'].tolist()
+                    quantity_sold.append(sum(sales))
+                elif column_data.count(product) == 1:
+                    sales = df[df['product'] == product]['quantity_sold'].values[0]
+                    quantity_sold.append(sales)
+            data = {'product': [i for i in products],
+                    'sales': [i for i in quantity_sold]}
+            result = pd.DataFrame(data)
+            res = result.groupby('product')['sales'].sum().sort_values(ascending=False)
+            print(res.to_string())
         elif option == '3':
             pass
         elif option == '4':
