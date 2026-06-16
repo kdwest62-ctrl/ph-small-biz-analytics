ed3fc72e001756e00621d534d0cbf536226c91e0
Kear Ibale <kdwest62@gmail.com>
2026-06-16 13:35:54 +0800

    Refactor sales calculation logic in tracker

--- sari-sari-sales-tracker.py
+++ sari-sari-sales-tracker.py
@@ -4,6 +4,20 @@ from pathlib import Path
 path = Path(input('CSV path: '))
 if path.exists():
     df = pd.read_csv(path)
+    column_data = df['product'].tolist()
+    products = []
+    for entry in column_data:
+        if entry not in products:
+            products.append(entry)
+    quantity_sold = []
+    for product in products:
+        if column_data.count(product) > 1:
+            prod = df[df['product'] == product]
+            sales = prod['quantity_sold'].tolist()
+            quantity_sold.append(sum(sales))
+        elif column_data.count(product) == 1:
+            sales = df[df['product'] == product]['quantity_sold'].values[0]
+            quantity_sold.append(sales)
     print('Menu')
     print('1. Data\n2. Sales\n3. Revenue\n4. Profit\n5. Inventory\n6. Exit')
     while True:
@@ -12,20 +26,6 @@ if path.exists():
             print(df.to_string())
             print('-' * 8)
         elif option == '2':
-            column_data = df['product'].tolist()
-            products = []
-            for entry in column_data:
-                if entry not in products:
-                    products.append(entry)
-            quantity_sold = []
-            for product in products:
-                if column_data.count(product) > 1:
-                    prod = df[df['product'] == product]
-                    sales = prod['quantity_sold'].tolist()
-                    quantity_sold.append(sum(sales))
-                elif column_data.count(product) == 1:
-                    sales = df[df['product'] == product]['quantity_sold'].values[0]
-                    quantity_sold.append(sales)
             data = {'product': [i for i in products],
                     'sales': [i for i in quantity_sold]}
             result = pd.DataFrame(data)
