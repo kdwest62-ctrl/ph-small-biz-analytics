277663af4bfdd01c72539dd3ae676b294b2b5739
Kear Ibale <kdwest62@gmail.com>
2026-07-01 07:47:30 +0800

    Add pandas import and CSV empty check

--- carinderia-usage.py
+++ carinderia-usage.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+import pandas as pd
 
 path = Path(input("CSV path: "))
 if path.exists():
     if path.suffix == ".csv":
-        print("Success")
+        df = pd.read_csv(path)
+        if df.empty:
+            print("CSV is empty")
+        else:
+            print("CSV is not empty")
     else:
         print("File not csv")
 else:
