838bb63cd0986ae5c21193b0b4cfed720ccbb042
Kear Ibale <kdwest62@gmail.com>
2026-07-01 07:27:59 +0800

    Refactor carinderia-usage.py to simplify functionality
    Removed menu options and related functionality from the script, retaining only the path existence check.

--- carinderia-usage.py
+++ carinderia-usage.py
@@ -1,36 +1,7 @@
-import pandas as pd
 from pathlib import Path
 
 path = Path(input("CSV path: "))
 if path.exists():
-    df = pd.read_csv(path)
-    menu = ["1. Data",
-            "2. Data Aggregation",
-            "3. Average Usage per Day Type",
-            "4. Forecast Ingredient Needs",
-            "5. Actual vs Predicted Usage",
-            "6. Data Simulation",
-            "7. Exit"]
-    for item in menu:
-        print(item)
-    while True:
-        option = input("Select option: ")
-        if option == "1":
-            print(df.to_string())
-        elif option == "2":
-            pass
-        elif option == "3":
-            pass
-        elif option == "4":
-            pass
-        elif option == "5":
-            pass
-        elif option == "6":
-            pass
-        elif option == "7":
-            print("Program closed")
-            break
-        else:
-            print("Invalid input")
+    print('Path exists')
 else:
     print("Path does not exist")
