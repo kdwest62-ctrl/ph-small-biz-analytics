9fb5d9deda497160e3b633c90a19aec2b2e9abfd
Kear Ibale <kdwest62@gmail.com>
2026-07-02 14:50:58 +0800

    Add date selection functionality to CSV usage

--- carinderia-usage.py
+++ carinderia-usage.py
@@ -8,7 +8,21 @@ if path.exists():
         if df.empty:
             print("CSV is empty")
         else:
-            print("CSV is not empty")
+            def get_dates(csv, user_choice):
+                if user_choice == 'a':
+                    return csv
+                elif user_choice == 'b':
+                    start_index = int(input('Start date (number): '))
+                    end_index = int(input('End date (number): '))
+                    end_index += 1
+                    result = csv.iloc[start_index:end_index].copy()
+                    return result
+                else:
+                    raise ValueError('Program must use either full or filtered CSV')
+            print(df.to_string())
+            choice = input('Use (a) full CSV or (b) select range of dates: ')
+            new_df = get_dates(df, choice)
+            print(new_df.to_string())
     else:
         print("File not csv")
 else:
