6a5449fd177135ea6c1ab35d048db370fd2a2e88
Kear Ibale <kdwest62@gmail.com>
2026-07-22 14:47:01 +0800

    Refactor ingredient comparison logic in carinderia-usage.py

--- carinderia-usage.py
+++ carinderia-usage.py
@@ -29,27 +29,34 @@ if path.exists():
             while True:
                 option = input('Option: ')
                 if option == '1':
-                    total = int(input('Number of ingredients to compare: '))
-                    averages = []
-                    ingredients = []
-                    count = 1
-                    while count <= total:
-                        ingredient = input(f'Ingredient {count}: ')
-                        check = pd.read_csv(path, nrows=0)
-                        if ingredient in check.columns:
-                            ingredients.append(ingredient)
-                            col_data = np.array(new_df[ingredient].tolist())
-                            average = np.mean(col_data)
-                            averages.append(round(average, 2))
-                            count += 1
-                        else:
-                            print('Ingredient does not exist')
-                    avg_usage = dict(zip(ingredients, averages))
-                    usage_ranked = create_rankings(avg_usage)
-                    data = {'ingredient': [i for i in usage_ranked.keys()],
-                            'average usage': [i for i in usage_ranked.values()]}
-                    result = pd.DataFrame(data)
-                    print(result.to_string())
+                    print('a. Show\nb. Compare')
+                    choice = input('Choose: ')
+                    if choice == 'a':
+                        ingredient = input('Ingredient: ')
+                        data = np.array(df[ingredient].tolist())
+                        print(f'Average usage: {np.mean(data)}')
+                    elif choice == 'b':
+                        total = int(input('Number of ingredients to compare: '))
+                        averages = []
+                        ingredients = []
+                        count = 1
+                        while count <= total:
+                            ingredient = input(f'Ingredient {count}: ')
+                            check = pd.read_csv(path, nrows=0)
+                            if ingredient in check.columns:
+                                ingredients.append(ingredient)
+                                col_data = np.array(new_df[ingredient].tolist())
+                                average = np.mean(col_data)
+                                averages.append(round(average, 2))
+                                count += 1
+                            else:
+                                print('Ingredient does not exist')
+                        avg_usage = dict(zip(ingredients, averages))
+                        usage_ranked = create_rankings(avg_usage)
+                        data = {'ingredient': [i for i in usage_ranked.keys()],
+                                'average usage': [i for i in usage_ranked.values()]}
+                        result = pd.DataFrame(data)
+                        print(result.to_string())
                 elif option == '2':
                     pass
                 elif option == '3':
