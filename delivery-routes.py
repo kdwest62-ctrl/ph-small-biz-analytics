4a2b54dea6d61d4bdafdca7b3111cb1012a39f8e
Kear Ibale <kdwest62@gmail.com>
2026-09-02 14:48:27 +0800

    Implement CSV reading and empty check
    Added functionality to read CSV and check if it's empty.

--- delivery-routes.py
+++ delivery-routes.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+import pandas as pd
 
 path = Path(input('CSV path: '))
 if path.exists():
     if path.suffix == '.csv':
-        pass
+        df = pd.read_csv(path)
+        if df.empty:
+            print('CSV is empty')
+        else:
+            pass
     else:
         print('File not CSV')
 else:
